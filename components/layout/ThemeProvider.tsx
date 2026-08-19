252b596f75a3731195e151ec695f8133026687a9
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2026-08-19 02:18:06 +0100

    Update ThemeProvider.tsx

--- components/layout/ThemeProvider.tsx
+++ components/layout/ThemeProvider.tsx
@@ -1,14 +1,8 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { Metadata } from 'next';
 
 
-export const metadata: Metadata = {
-  verification: {
-    google: 'exAAHpiMsljDHyf5dcUDz6KESuX79fZMvd1HLDsb0p0',
-  },
-};
 
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
